1561c3cc23022fec7d56d941b6a141407d285315
Elkin Aguilar <aguilar.elkin99@gmail.com>
2023-10-01 16:52:38 -0500

    first commit

---
+++ basicclient/src/test/java/com/angular/basicclient/BasicclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angular.basicclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
